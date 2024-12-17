458940814b80cbca13c2a7c6f99f31d7db1ab578
darkirby <darkirby.tuta.com>
2024-12-17 10:30:33 +0100

    Add bootstrap5, postcss with autoprefixer, stylelint scss config and correct hook bug

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,8 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// start the Stimulus application
+import './bootstrap';
+
+// import boostrap plugins (you can specify which plugins you need)
+import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import './styles/global.scss';
