51220f75ace5e174dafcbb833d6f3269593d05da
darkirby <darkirby.tuta.com>
2025-01-28 22:11:09 +0100

    Remove app_ prefix from all route names

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,20 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_default_index')]
+    #[Route('/', name: 'default_index')]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', []);
-        //    return $this->redirectToRoute('app_default_home');
+        //    return $this->redirectToRoute('default_home');
     }
 
-    // #[Route('/home', name: 'app_default_home')]
+    // #[Route('/home', name: 'default_home')]
     // public function home(): Response
     // {
     //     return $this->render('default/home.html.twig', []);
     // }
 
-    #[Route('/example', name: 'app_default_example')]
+    #[Route('/example', name: 'default_example')]
     public function example(): Response
     {
         return $this->render('default/example.html.twig', []);
