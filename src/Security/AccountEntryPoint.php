6f23583b0bd018501b63ac460400d61d8645a869
darkirby <darkirby.tuta.com>
2025-06-06 11:59:37 +0200

    Generate links to the hub account managment + improve scss

--- src/Security/AccountEntryPoint.php
+++ src/Security/AccountEntryPoint.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Security;
 
+use App\Service\AccountUrlGenerator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -11,21 +12,14 @@ use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface
 
 class AccountEntryPoint implements AuthenticationEntryPointInterface
 {
-    public function __construct(private string $hubLoginRoute)
+    public function __construct(private AccountUrlGenerator $accountUrlGenerator)
     {
     }
 
     public function start(Request $request, ?AuthenticationException $authException = null): RedirectResponse
     {
-        if (str_starts_with($this->hubLoginRoute, '/')) {
-            $serverBaseUrl = $request->getSchemeAndHttpHost();
-            $accountUrl = $serverBaseUrl . $this->hubLoginRoute;
-        } else {
-            $accountUrl = $this->hubLoginRoute;
-        }
-
         $request->getSession()->set('_login_target_path', $request->getUri());
 
-        return new RedirectResponse($accountUrl);
+        return new RedirectResponse($this->accountUrlGenerator->getHubLoginUrl());
     }
 }
