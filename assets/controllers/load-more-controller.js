4f2a4403d756f0984175410d858e1ace9e465040
darkirby <darkirby.tuta.com>
2025-11-17 23:04:30 +0100

    Load more display a message and a button in case of error or redirect

--- assets/controllers/load-more-controller.js
+++ assets/controllers/load-more-controller.js
@@ -6,10 +6,20 @@ export default class extends DynamicButtonController {
     fetch(this.urlValue, {
       method: 'GET',
       headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      redirect: 'manual',
     })
-      .then((response) => response.text())
+      .then((response) => {
+        if (response.type === 'opaqueredirect' || response.redirected || !response.ok) {
+          throw new Error('Invalid ou redirect response.');
+        }
+        return response.text();
+      })
       .then((html) => {
         this.element.outerHTML = html;
+      })
+      .catch(() => {
+        this.element.outerHTML =
+          '<div class="text-center text-secondary fs-6 fst-italic">Erreur de chargement<br><a href="" class="my-2 btn btn-outline-secondary" data-turbo="false">Recharger la page</a></div>';
       });
   }
 }
