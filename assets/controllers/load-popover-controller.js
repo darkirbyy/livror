793ce9954e566dea72b1a8d5843b7de92f09cd1b
darkirby <darkirby.tuta.com>
2026-01-20 18:54:33 +0100

    Add popover on user cell

---
+++ assets/controllers/load-popover-controller.js
@@ -0,0 +1,24 @@
+import { Controller } from '@hotwired/stimulus';
+import { Popover } from 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+/**
+ * Stimulus controller that CAN be placed on any tag, to enable any popover nested inside it.
+ */
+export default class extends Controller {
+  connect() {
+    // authorize data-turbo-frame on a tag for the sanitizer
+    const myDefaultAllowList = bootstrap.Tooltip.Default.allowList;
+    myDefaultAllowList.a.push('data-turbo-frame');
+
+    // load each popover
+    const popoverTriggerList = this.element.querySelectorAll('[data-bs-toggle="popover"]');
+    popoverTriggerList.forEach((item) => {
+      new Popover(item, {
+        trigger: 'focus',
+        placement: 'top',
+        html: true,
+      });
+    });
+  }
+}
