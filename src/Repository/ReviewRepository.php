3ed789e16a4eff24eccf38e12937fbe74df040eb
darkirby <darkirby.tuta.com>
2026-01-23 12:27:57 +0100

    Add type of game filter on review page

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -9,6 +9,7 @@ use App\Entity\Main\Review;
 use App\Enum\DateFieldEnum;
 use App\Service\QueryParamHelper;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ReviewRepository extends ServiceEntityRepository
@@ -22,11 +23,11 @@ class ReviewRepository extends ServiceEntityRepository
     {
         // Define allowed sort and filter parameters and the conversion to the doctrine field
         $sortsConversion = ['name' => 'g.name', 'rating' => 'r.rating', 'hourSpend' => 'r.hourSpend', 'firstPlay' => 'r.firstPlay'];
-        $filtersConversion = [];
+        $filtersConversion = ['typeGame' => ''];
 
         // Validate and complete the parameters
         $this->queryParamHelper->load($queryParam, 'review-index');
-        $this->queryParamHelper->defaults($queryParam, ['name' => 'asc'], []);
+        $this->queryParamHelper->defaults($queryParam, ['name' => 'asc'], ['typeGame' => '']);
         $this->queryParamHelper->validate($queryParam, array_keys($sortsConversion), array_keys($filtersConversion));
         $this->queryParamHelper->save($queryParam, 'review-index');
 
@@ -34,8 +35,9 @@ class ReviewRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('r');
         $qb->leftJoin('r.game', 'g')->where('r.userId = :userId')->setParameter('userId', $userId);
 
-        // Apply the query param but filters
+        // Apply the query param
         $this->queryParamHelper->applyButFiltersToQb($queryParam, $qb, $sortsConversion);
+        $this->applyFiltersToQb($queryParam, $qb);
 
         // Execute and fetch the query
         return $qb->getQuery()->getResult();
@@ -69,5 +71,15 @@ class ReviewRepository extends ServiceEntityRepository
 
         // Execute and fetch the query
         return $qb->getQuery()->getResult();
+    }
+
+    private function applyFiltersToQb(QueryParam $queryParam, QueryBuilder $qb): QueryBuilder
+    {
+        // Filter logic : type of game
+        if (array_key_exists('typeGame', $queryParam->filters) && !empty($queryParam->filters['typeGame'])) {
+            $qb->andWhere('g.typeGame IN (:typeGame)')->setParameter('typeGame', $queryParam->filters['typeGame']);
+        }
+
+        return $qb;
     }
 }
